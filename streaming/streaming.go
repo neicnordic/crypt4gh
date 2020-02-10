bea9749d6257ef737e1e4cf78e61aed0564df3a4
dtitov <dmytro.titov@gmail.com>
2020-02-10 22:36:33 +0100

    Use bytes.Buffer

--- streaming/streaming.go
+++ streaming/streaming.go
@@ -3,6 +3,7 @@ package streaming
 import (
 	"../model/body"
 	"../model/headers"
+	"bytes"
 	"errors"
 	"io"
 )
@@ -13,15 +14,13 @@ type Crypt4GHInternalReader struct {
 	header                               headers.Header
 	dataEncryptionParameterHeaderPackets []headers.DataEncryptionParametersHeaderPacket
 	encryptedSegmentSize                 int
-
-	buffer    []int
-	bytesRead int
+	lastDecryptedSegment                 int
+	buffer                               bytes.Buffer
 }
 
 func NewCrypt4GHInternalReader(reader io.Reader, privateKey [32]byte) (*Crypt4GHInternalReader, error) {
 	crypt4GHInternalReader := Crypt4GHInternalReader{}
 	header, err := headers.NewHeader(reader, privateKey)
-	crypt4GHInternalReader.reader = reader
 	if err != nil {
 		return nil, err
 	}
@@ -37,61 +36,46 @@ func NewCrypt4GHInternalReader(reader io.Reader, privateKey [32]byte) (*Crypt4GH
 		}
 	}
 	crypt4GHInternalReader.encryptedSegmentSize = firstDataEncryptionParametersHeader.EncryptedSegmentSize
+	crypt4GHInternalReader.lastDecryptedSegment = -1
+	crypt4GHInternalReader.reader = reader
 	return &crypt4GHInternalReader, nil
 }
 
 func (c *Crypt4GHInternalReader) Read(p []byte) (n int, err error) {
-	readByte, err := c.ReadByte()
-	if err != nil {
-		return 0, err
-	}
-	p[0] = readByte
-	i := 1
-	for ; i < len(p); i++ {
-		readByte, err := c.ReadByte()
-		if err != nil {
-			return i, err
-		}
-		p[i] = readByte
-	}
-	return i, nil
-}
-
-func (c *Crypt4GHInternalReader) ReadByte() (byte, error) {
-	if len(c.buffer) == 0 || len(c.buffer) == c.bytesRead {
+	if c.buffer.Len() == 0 {
 		err := c.fillBuffer()
 		if err != nil {
 			return 0, err
 		}
 	}
-	nextByte := c.buffer[c.bytesRead]
-	if nextByte == -1 {
-		return 0, io.EOF
+	return c.buffer.Read(p)
+}
+
+func (c *Crypt4GHInternalReader) ReadByte() (byte, error) {
+	if c.buffer.Len() == 0 {
+		err := c.fillBuffer()
+		if err != nil {
+			return 0, err
+		}
 	}
-	c.bytesRead++
-	return byte(nextByte), nil
+	return c.buffer.ReadByte()
 }
 
 func (c *Crypt4GHInternalReader) fillBuffer() error {
-	c.bytesRead = 0
 	encryptedSegmentBytes := make([]byte, c.encryptedSegmentSize)
 	read, err := c.reader.Read(encryptedSegmentBytes)
 	if err != nil {
 		return err
 	}
 	if read == 0 {
-		for i := range c.buffer {
-			c.buffer[i] = -1
-		}
+		c.buffer.Truncate(0)
 	} else {
 		segment, err := body.NewSegment(encryptedSegmentBytes[:read], c.dataEncryptionParameterHeaderPackets)
 		if err != nil {
 			return err
 		}
-		c.buffer = make([]int, len(segment.UnencryptedData))
-		for i, b := range segment.UnencryptedData {
-			c.buffer[i] = int(b)
-		}
+		c.buffer.Grow(len(segment.UnencryptedData))
+		c.buffer.Write(segment.UnencryptedData)
 	}
 	return nil
 }
