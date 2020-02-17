89967052e6d2ae315a135208535716cf5e85aa09
dtitov <dmytro.titov@gmail.com>
2020-02-17 13:25:00 +0100

    Improve coverage of body_test.go

--- model/body/body_test.go
+++ model/body/body_test.go
@@ -15,7 +15,7 @@ var dataEncryptionParametersHeaderPacket = headers.DataEncryptionParametersHeade
 
 var nonce = [12]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 
-func TestSegment_MarshalBinary(t *testing.T) {
+func TestSegment_MarshalBinaryWithNonce(t *testing.T) {
 
 	segment := Segment{
 		DataEncryptionParametersHeaderPackets: []headers.DataEncryptionParametersHeaderPacket{dataEncryptionParametersHeaderPacket},
@@ -27,6 +27,20 @@ func TestSegment_MarshalBinary(t *testing.T) {
 		t.Error(err)
 	}
 	if hex.EncodeToString(binary) != "0102030405060708090a0b0cf2cc363c27248a881b102cd36f678e1d88c47076e0d263ad45759c03" {
+		t.Fail()
+	}
+}
+
+func TestSegment_MarshalBinaryWithoutNonce(t *testing.T) {
+	segment := Segment{
+		DataEncryptionParametersHeaderPackets: []headers.DataEncryptionParametersHeaderPacket{dataEncryptionParametersHeaderPacket},
+		UnencryptedData:                       nonce[:],
+	}
+	binary, err := segment.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	if binary == nil {
 		t.Fail()
 	}
 }
--- streaming/streaming_test.go
+++ streaming/streaming_test.go
@@ -12,63 +12,71 @@ import (
 func TestReencryption(t *testing.T) {
 	inFile, err := os.Open("../test/sample.txt")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	buf := bytes.Buffer{}
 	keyFile, err := os.Open("../test/ssh-ed25519-enc.sec.pem")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	writerPrivateKey, err := keys.ReadPrivateKey(keyFile, []byte("123123"))
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	keyFile, err = os.Open("../test/crypt4gh-x25519-enc.pub.pem")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	readerPublicKey, err := keys.ReadPublicKey(keyFile)
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	writer, err := NewCrypt4GHWriter(&buf, writerPrivateKey, readerPublicKey, nil)
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	_, err = io.Copy(writer, inFile)
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	err = inFile.Close()
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	err = writer.Close()
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 
 	keyFile, err = os.Open("../test/crypt4gh-x25519-enc.sec.pem")
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	readerSecretKey, err := keys.ReadPrivateKey(keyFile, []byte("password"))
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
 	reader, err := NewCrypt4GHReader(&buf, readerSecretKey, nil)
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
-	discarded, err := reader.Discard(4)
+	_, err = reader.Discard(4)
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
-	println(discarded)
 	all, err := ioutil.ReadAll(reader)
 	if err != nil {
-		panic(err)
+		t.Error(err)
 	}
-	s := string(all)
-	println(s)
+	inFile, err = os.Open("../test/sample.txt")
+	if err != nil {
+		t.Error(err)
+	}
+	inBytes, err := ioutil.ReadAll(inFile)
+	if err != nil {
+		t.Error(err)
+	}
+	if bytes.Compare(all, inBytes[4:]) != 0 {
+		t.Fail()
+	}
 }
