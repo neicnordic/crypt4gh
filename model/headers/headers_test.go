3490c163dde8020679c6fcef432845f181f66aea
dtitov <dmytro.titov@gmail.com>
2020-02-17 13:29:42 +0100

    Improve coverage of headers_test.go

--- model/headers/headers_test.go
+++ model/headers/headers_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 )
 
-func TestHeaderMarshalling(t *testing.T) {
+func TestHeaderMarshallingWithNonce(t *testing.T) {
 	keyFile, err := os.Open("../../test/ssh-ed25519-enc.sec.pem")
 	if err != nil {
 		panic(err)
@@ -93,6 +93,63 @@ func TestNewHeader(t *testing.T) {
 	}
 	if fmt.Sprintf("%v", header) != "&{[99 114 121 112 116 52 103 104] 1 2 [{[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] 108 0 <nil> {65564 {0} 0 [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]}} {[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0] 100 0 <nil> {{1} 3 [1 2 3]}}]}" {
 		t.Fail()
+	}
+}
+
+func TestHeaderMarshallingWithoutNonce(t *testing.T) {
+	keyFile, err := os.Open("../../test/ssh-ed25519-enc.sec.pem")
+	if err != nil {
+		panic(err)
+	}
+	writerPrivateKey, err := keys.ReadPrivateKey(keyFile, []byte("123123"))
+	if err != nil {
+		panic(err)
+	}
+	keyFile, err = os.Open("../../test/crypt4gh-x25519-enc.pub.pem")
+	if err != nil {
+		panic(err)
+	}
+	readerPublicKey, err := keys.ReadPublicKey(keyFile)
+	if err != nil {
+		panic(err)
+	}
+	magic := [8]byte{}
+	copy(magic[:], MagicNumber)
+	header := Header{
+		MagicNumber:       magic,
+		Version:           1,
+		HeaderPacketCount: 2,
+		HeaderPackets: []HeaderPacket{{
+			WriterPrivateKey:       writerPrivateKey,
+			ReaderPublicKey:        readerPublicKey,
+			PacketLength:           10,
+			HeaderEncryptionMethod: X25519ChaCha20IETFPoly1305,
+			EncryptedHeaderPacket: DataEncryptionParametersHeaderPacket{
+				EncryptedSegmentSize: 10,
+				PacketType:           PacketType{DataEncryptionParameters},
+				DataEncryptionMethod: ChaCha20IETFPoly1305,
+				DataKey:              [32]byte{},
+			},
+		},
+			{
+				WriterPrivateKey:       writerPrivateKey,
+				ReaderPublicKey:        readerPublicKey,
+				PacketLength:           10,
+				HeaderEncryptionMethod: X25519ChaCha20IETFPoly1305,
+				EncryptedHeaderPacket: DataEditListHeaderPacket{
+					PacketType:    PacketType{DataEditList},
+					NumberLengths: 3,
+					Lengths:       []uint64{1, 2, 3},
+				},
+			},
+		},
+	}
+	marshalledHeader, err := header.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	if marshalledHeader == nil {
+		t.Error(err)
 	}
 }
 
