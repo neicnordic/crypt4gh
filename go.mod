c5a68497a450cda792ca4f89a467a3b721244924
Stefan Negru <stefan@blankdots.com>
2022-11-29 18:24:57 +0200

    drop promptui & update aurora dependency
    - promptui has not bee updated in a while and aurora was using v2
    - fix file overwrite

--- go.mod
+++ go.mod
@@ -5,13 +5,11 @@ go 1.17
 require (
 	filippo.io/edwards25519 v1.0.0
 	github.com/dchest/bcrypt_pbkdf v0.0.0-20150205184540-83f37f9c154a
+	github.com/hashicorp/go-version v1.6.0
 	github.com/jessevdk/go-flags v1.5.0
-	github.com/logrusorgru/aurora v2.0.3+incompatible
-	github.com/manifoldco/promptui v0.9.0
-	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064
+	github.com/logrusorgru/aurora/v4 v4.0.0
+	golang.org/x/crypto v0.3.0
+	golang.org/x/term v0.2.0
 )
 
-require (
-	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-)
+require golang.org/x/sys v0.2.0 // indirect
---
+++ internal/cli/cli.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"github.com/logrusorgru/aurora/v4"
+	"golang.org/x/term"
+)
+
+// YesNoPrompt asks yes/no questions using the label.
+func YesNoPrompt(label string, def bool) bool {
+	choices := "Yes/no"
+	if !def {
+		choices = "yes/No"
+	}
+
+	r := bufio.NewReader(os.Stdin)
+	var s string
+
+	for {
+		fmt.Fprintf(os.Stdout, "%s (%s) ", aurora.Bold(label), aurora.Underline(choices))
+		s, _ = r.ReadString('\n')
+		s = strings.TrimSpace(s)
+		if s == "" {
+			return def
+		}
+		s = strings.ToLower(s)
+		if s == "y" || s == "yes" {
+			return true
+		}
+		if s == "n" || s == "no" {
+			return false
+		}
+	}
+}
+
+func PasswordPrompt(label string) (string, error) {
+	fmt.Fprint(os.Stdout, aurora.Bold(label+" "))
+	pwd, err := term.ReadPassword(int(syscall.Stdin))
+	fmt.Println()
+
+	return string(pwd), err
+}
