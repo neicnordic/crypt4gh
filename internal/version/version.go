0cdd2a01bdb29fa76423695675400625d45d4716
Stefan Negru <stefan@blankdots.com>
2023-05-16 07:59:54 +0300

    bump to version 1.7.5

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,7 +7,7 @@ import (
 )
 
 // The version in the current branch
-var Version = "1.7.3"
+var Version = "1.7.5"
 
 // If this is "" (empty string) then it means that it is a final release.
 // Otherwise, this is a pre-release e.g. "dev", "beta", "rc1", etc.
